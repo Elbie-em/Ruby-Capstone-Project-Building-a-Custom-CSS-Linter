7bb019be5d7ce05531422b079e8fae1d578cc5b6
elbie-em <emoonga1993@gmail.com>
2020-03-31 22:14:30 +0200

    added white_trailing_space_error method

--- lib/css_inspector.rb
+++ lib/css_inspector.rb
@@ -10,16 +10,25 @@ class CSSInspectorTool
   
   def after_colon_space_error?(f_string,f_index)
     if f_string.match?(/(:\w+)/)
-      puts ' NoSpacAfterColonError'.red + ': no space after colon ":" at' + " line:[:#{f_index}:]".yellow
+      puts ' NoSpacAfterColonError'.red + ': no space after colon ":" on' + " line:[:#{f_index}:]".yellow
     else
       return
     end
   end
+
   def before_colon_space_error?(f_string,f_index)
     if f_string.match?(/( :)/)
-      puts ' SpaceBeforeColonError'.red + ': space before colon " :" at' + " line:[:#{f_index}:]".yellow
+      puts ' SpaceBeforeColonError'.red + ': space before colon " :" on' + " line:[:#{f_index}:]".yellow
     else
       return
+    end
+  end
+
+  def white_trailing_space_error?(f_string,f_index)
+    if f_string.match?(/([^\s]\n)$/) || f_string.match?(/^\n$/)
+      return
+    else
+      puts ' WhiteTrailingSpaceError'.red + ': trailing white space on' + " line:[:#{f_index}:]".yellow
     end
   end
 
@@ -27,6 +36,7 @@ class CSSInspectorTool
     @file_details_array.each_with_index do |f_string,f_index|
       after_colon_space_error?(f_string,f_index + 1)
       before_colon_space_error?(f_string,f_index + 1)
+      white_trailing_space_error?(f_string,f_index + 1)
     end
   end
 
