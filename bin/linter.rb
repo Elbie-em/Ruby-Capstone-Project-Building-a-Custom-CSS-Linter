34a9e234aa6f0d790bd5e97914efacf4de1ee423
elbie-em <emoonga1993@gmail.com>
2020-03-31 18:59:32 +0200

    reading user_file functionality complete

--- bin/linter.rb
+++ bin/linter.rb
@@ -4,6 +4,10 @@ user_file_path = ARGV
 
 user_file_path = user_file_path.shift
 
+puts "Checking '#{user_file_path}' for errors..."
+
 user_file = File.open(user_file_path)
 
-user_file.read
+user_file_details = user_file.readlines
+
+puts user_file_details
