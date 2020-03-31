a52b0033458086408b764a27be32bbef2f9c8b9c
elbie-em <emoonga1993@gmail.com>
2020-03-31 21:35:30 +0200

    added after_colon_space_error method, added inspect file method

--- bin/linter.rb
+++ bin/linter.rb
@@ -1,5 +1,5 @@
 require './lib/user_file.rb'
-require '/lib/css_inspector.rb'
+require './lib/css_inspector.rb'
 
 # importing users file for command line interface
 
@@ -14,4 +14,6 @@ user_file = UserFile.new(user_file_path)
 user_file_details = user_file.read_file_elements(user_file)
 
 inspector_tool = CSSInspectorTool.new(user_file_details)
+
+inspector_tool.inspect_file
 
--- lib/css_inspector.rb
+++ lib/css_inspector.rb
@@ -1,7 +1,28 @@
+require 'strscan'
+require 'colorize'
 class CSSInspectorTool
-  def initialize(file_details)
-    @file_details = file_details
+  attr_reader :file_details
+
+  def initialize(file_details_arr)
+    @file_details_array = file_details_arr
   end
+
+  
+  def after_colon_space_error?(f_string,f_index)
+    if f_string.match?(/(:\w+)/)
+      puts ' NoSpacAfterColonError'.red + ': no space after colon ":" at' + " line:[:#{f_index}:]".yellow
+    else
+      return
+    end
+  end
+  
+
+  def inspect_file
+    @file_details_array.each_with_index do |f_string,f_index|
+      after_colon_space_error?(f_string,f_index + 1)
+    end
+  end
+
 
   
 end
