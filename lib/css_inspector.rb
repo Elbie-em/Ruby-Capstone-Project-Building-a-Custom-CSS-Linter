c0b12b52ce08297ad2d86074883c6098b4365c47
elbie-em <emoonga1993@gmail.com>
2020-04-01 00:58:36 +0200

    added user interaction tweaks

--- lib/css_inspector.rb
+++ lib/css_inspector.rb
@@ -1,4 +1,5 @@
 require './lib/module_errors.rb'
+require 'colorize'
 
 class CSSInspectorTool
   include Errors
@@ -8,16 +9,24 @@ class CSSInspectorTool
     @file_details_array = file_details_arr
   end
 
+  public
   def inspect_file
     @file_details_array.each_with_index do |f_string,f_index|
       after_colon_space_error?(f_string,f_index + 1)
+      sleep(0.1)
       before_colon_space_error?(f_string,f_index + 1)
+      sleep(0.1)
       white_trailing_space_error?(f_string,f_index + 1)
+      sleep(0.1)
       indentation_error?(f_string,f_index + 1)
+      sleep(0.1)
       space_before_brace_error?(f_string,f_index + 1)
+      sleep(0.1)
       missing_ending_semicolon_error?(f_string,f_index + 1)  
+      sleep(0.1)
       space_before_semicolon_error?(f_string,f_index + 1)
     end
+  puts "File Check Complete".green
   end
 
 end
