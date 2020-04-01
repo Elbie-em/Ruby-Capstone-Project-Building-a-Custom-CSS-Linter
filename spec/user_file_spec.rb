351a50928766751702e431e87da6ce347d029102
elbie-em <emoonga1993@gmail.com>
2020-04-02 01:47:28 +0200

    added test for user_file.rb intialize constructor method and read_file elements method

---
+++ spec/user_file_spec.rb
@@ -0,0 +1,18 @@
+require './lib/user_file.rb'
+
+RSpec.describe UserFile do
+  let(:user_file) { UserFile.new('./files/stylesheet.css') }
+
+
+  describe '#initialize' do
+    it 'declares path of the file we want to inspect' do
+      expect(user_file.user_file_path).to eql('./files/stylesheet.css')
+    end
+  end
+
+  describe '.read_file_elements' do
+    it 'Opens the file and traverses through its content returning an array of content' do
+      expect(user_file.read_file_elements(user_file).class).to eql(Array)
+    end
+  end
+end
